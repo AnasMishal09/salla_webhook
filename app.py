b518bfb75db895782b8b2b4896ea81d011eee15a
AnasMishal09 <anascaptul@gmail.com>
2025-07-26 16:34:42 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,15 @@ app = Flask(__name__)
 def index():
     return "✅ Flask app is running on Render!"
 
+# ✅ Webhook route for Salla
+@app.route('/webhook', methods=['POST'])
+def salla_webhook():
+    # Log the incoming webhook payload
+    data = request.get_json()
+    print("✅ Webhook received from Salla:")
+    print(data
+
+
 if __name__ == '__main__':
     import os
     port = int(os.environ.get("PORT", 10000))  # Render usually sets this to 10000
